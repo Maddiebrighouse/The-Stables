91131c71517148f48a7a63bd36bf93333fa38831
Madeleine Brighouse-Mayo <madeleinebrighouse@gmail.com>
2020-04-09 10:20:04 +1200

    Started on a time counter, not working yet.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,29 @@
 import React from "react";
-//import Moment from "react-moment";
+import Moment from "react-moment";
+import moment from "moment";
 
 import "./Header.scss";
 
 const Header = () => {
+  const startTime = "2020-03-19T08:00:00-12:00";
+  // Days
+  var now = moment(new Date()); //todays date
+  var end = moment("2020-03-19"); // another date
+  var duration = moment.duration(now.diff(end));
+  var days = Math.floor(duration.asDays());
+
+  // Hours
+  var now = moment(moment()); //todays date
+  var end = moment("T07:30:53.000"); // another date
+  var duration = moment.duration(now.diff(end));
+  var hours = Math.floor(duration.asHours());
+
+  console.log(days);
   return (
     <div className="header-container">
       <h1 className="title">Isolation at The Stables</h1>
+      <p>{`${days} days`}</p>
+      <p>{`${hours} days`}</p>
       <h3>13 days 12 hours 2 minutes 20 seconds</h3>
     </div>
   );
