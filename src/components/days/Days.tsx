0379c96144886a629708b176d65edd71828ee66d
Madeleine Brighouse-Mayo <madeleinebrighouse@gmail.com>
2020-04-11 16:42:31 +1200

    Css chages to positions and padding css

--- src/components/days/Days.tsx
+++ src/components/days/Days.tsx
@@ -5,16 +5,126 @@ import "./Days.scss";
 const Days = () => {
   return (
     <div className="days-container">
-      <a href="/day1">1</a>
-      <a href="/day1">2</a>
-      <a href="/day1">3</a>
-      <a href="/day1">4</a>
-      <a href="/day1">5</a>
-      <a href="/day1">6</a>
-      <a href="/day1">7</a>
-      <a href="/day1">8</a>
-      <a href="/day1">9</a>
-      <a href="/day1">10</a>
+      <a className="day-square" href="/day1">
+        1
+      </a>
+      <a className="day-square" href="/day1">
+        2
+      </a>
+      <a className="day-square" href="/day1">
+        3
+      </a>
+      <a className="day-square" href="/day1">
+        4
+      </a>
+      <a className="day-square" href="/day1">
+        5
+      </a>
+      <a className="day-square" href="/day1">
+        6
+      </a>
+      <a className="day-square" href="/day1">
+        7
+      </a>
+      <a className="day-square" href="/day1">
+        8
+      </a>
+      <a className="day-square" href="/day1">
+        9
+      </a>
+      <a className="day-square" href="/day1">
+        10
+      </a>
+      <a className="day-square" href="/day1">
+        1
+      </a>
+      <a className="day-square" href="/day1">
+        2
+      </a>
+      <a className="day-square" href="/day1">
+        3
+      </a>
+      <a className="day-square" href="/day1">
+        4
+      </a>
+      <a className="day-square" href="/day1">
+        5
+      </a>
+      <a className="day-square" href="/day1">
+        6
+      </a>
+      <a className="day-square" href="/day1">
+        7
+      </a>
+      <a className="day-square" href="/day1">
+        8
+      </a>
+      <a className="day-square" href="/day1">
+        9
+      </a>
+      <a className="day-square" href="/day1">
+        10
+      </a>
+      <a className="day-square" href="/day1">
+        1
+      </a>
+      <a className="day-square" href="/day1">
+        2
+      </a>
+      <a className="day-square" href="/day1">
+        3
+      </a>
+      <a className="day-square" href="/day1">
+        4
+      </a>
+      <a className="day-square" href="/day1">
+        5
+      </a>
+      <a className="day-square" href="/day1">
+        6
+      </a>
+      <a className="day-square" href="/day1">
+        7
+      </a>
+      <a className="day-square" href="/day1">
+        8
+      </a>
+      <a className="day-square" href="/day1">
+        9
+      </a>
+      <a className="day-square" href="/day1">
+        10
+      </a>
+      <a className="day-square" href="/day1">
+        1
+      </a>
+      <a className="day-square" href="/day1">
+        2
+      </a>
+      <a className="day-square" href="/day1">
+        3
+      </a>
+      <a className="day-square" href="/day1">
+        4
+      </a>
+      <a className="day-square" href="/day1">
+        5
+      </a>
+      <a className="day-square" href="/day1">
+        6
+      </a>
+      <a className="day-square" href="/day1">
+        7
+      </a>
+      <a className="day-square" href="/day1">
+        8
+      </a>
+      <a className="day-square" href="/day1">
+        9
+      </a>
+      <a className="day-square" href="/day1">
+        10
+      </a>
     </div>
   );
 };
