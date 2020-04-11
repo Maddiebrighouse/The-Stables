f9c5984c13e22e0dd473fdd7679fb569a648537b
Madeleine Brighouse-Mayo <madeleinebrighouse@gmail.com>
2020-04-11 16:42:04 +1200

    Create routes and css for nav

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -4,11 +4,16 @@ import { Link } from "react-router-dom";
 import "./Nav.scss";
 
 const Nav = () => {
-  return;
-  <div className="nav">
-    <Link to="/who">who</Link>
-    <Link to="/days">days</Link>
-  </div>;
+  return (
+    <div className="nav">
+      <Link className="link" to="/who">
+        who
+      </Link>
+      <Link className="link" to="/days">
+        days
+      </Link>
+    </div>
+  );
 };
 
 export default Nav;
