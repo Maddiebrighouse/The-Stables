3bcbd5fb16a640442e1f88c35c2e857b073ca51a
Madeleine Brighouse-Mayo <madeleinebrighouse@gmail.com>
2020-04-14 20:18:12 +1200

    Database connected using REST till I get graphql

---
+++ src/db/db.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+
+const uri = `mongodb+srv://${process.env.USERNAMEDB}:${
+  process.env.PASSWORDDB
+}@isolationatthestables-fn9tr.mongodb.net/test?retryWrites=true&w=majority`;
+
+module.exports = {
+  getJobs: async (values) => {
+    const clientPromise = MongoClient.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    const client = await Promise.resolve(clientPromise);
+    const db = client.db("IsolationStables");
+    const output = await db
+      .collection("IsolationAtTheStables")
+      .find(values)
+      .sort()
+      .toArray();
+    client.close();
+    return output;
+  },
+};
--- src/server/server.js
+++ src/server/server.js
@@ -3,7 +3,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const server = express();
+const photo = require("./routes");
 
 server.use(bodyParser.json());
 server.use(express.static(path.join(__dirname, "../../public")));
+
+server.use("/api/v1/", photo);
+
 module.exports = server;
