455f0e4510a3c9bdbc05a2459710b511e8b22f21
Madeleine Brighouse-Mayo <madeleinebrighouse@gmail.com>
2020-04-16 22:10:40 +1200

    Removed Apollo

--- src/index.js
+++ src/index.js
@@ -1,27 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ApolloProvider } from "react-apollo";
-import { ApolloClient } from "apollo-client";
-import { createHttpLink } from "apollo-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
 
 import "./index.scss";
 import App from "./App";
 
-const httpLink = createHttpLink({
-  uri: "http://localhost:4000"
-});
-
-const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache()
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-  ReactDOM.render(
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>,
-    document.getElementById("app")
-  );
-});
+ReactDOM.render(<App />, document.getElementById("app"));
