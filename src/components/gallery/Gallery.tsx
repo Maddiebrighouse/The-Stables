c803e29b8650f7891ffdc2100374afb8cecc5d9f
Madeleine Brighouse-Mayo <madeleinebrighouse@gmail.com>
2020-04-16 22:11:22 +1200

    added funtion ality to reuse componet for days

--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -7,33 +7,50 @@ type State = {
   err: string;
   active: boolean;
   photos: PhotoType[];
+  showDay: boolean;
+};
+
+type Props = {
+  match: any;
 };
 
 interface PhotoType {
-  _id: IDBDatabase;
+  _id: String;
   date: String;
   tags: [String];
+  video: String;
   image: String;
   day: Number;
   comment: String;
 }
-class Gallery extends React.Component<{}, State> {
-  constructor({}) {
-    super({});
+class Gallery extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
     this.toggleClass = this.toggleClass.bind(this);
     this.state = {
       active: false,
       photos: [],
       err: "",
+      showDay: true,
     };
   }
 
   componentDidMount() {
-    request.get(`/api/v1`).then((res) => {
-      this.setState({
-        photos: res.body.value,
+    const dayNumber = parseInt(this.props.match.params.day);
+    if (dayNumber > 0) {
+      request.get(`/api/v1/days/${dayNumber}`).then((res) => {
+        this.setState({
+          photos: res.body.value,
+          showDay: false,
+        });
       });
-    });
+    } else {
+      request.get(`/api/v1`).then((res) => {
+        this.setState({
+          photos: res.body.value,
+        });
+      });
+    }
   }
 
   toggleClass() {
@@ -50,12 +67,13 @@ class Gallery extends React.Component<{}, State> {
         </button>
         {this.state.active && <Filter />}
         <div className="gallery-container">
-          {photos.map((photo, index) => {
+          {photos.map((photo) => {
             return (
-              <div className="gallery-item">
+              <div key={photo._id} className="gallery-item">
                 <img src={`${photo.image}`} alt="placeholder" />
+                {photo.video && <video src={`${photo.video}`} />}
                 <div className="comment">
-                  <p className="day">day {photo.day}</p>
+                  {this.state.showDay && <p className="day">day {photo.day}</p>}
                   <p className="day-comment">{photo.comment}</p>
                 </div>
               </div>
