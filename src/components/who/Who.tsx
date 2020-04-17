641d2fc0f0a45f22eb0aacf8168a211de4a538c5
Madeleine Brighouse-Mayo <madeleinebrighouse@gmail.com>
2020-04-17 15:53:09 +1200

    added new text and update styles

--- src/components/who/Who.tsx
+++ src/components/who/Who.tsx
@@ -8,7 +8,9 @@ const Who = () => {
       <div className="exit">
         <a href="/">X</a>
       </div>
-      <h1>The Stables is home to 5 flat mates and a giant horse lantern...</h1>
+      <h1 className="who-header">
+        The Stables is home to 5 flat mates and a giant horse lantern...
+      </h1>
       <p>
         In the current circumstance, people living here at The Stables are...
       </p>
@@ -27,10 +29,20 @@ const Who = () => {
         <br />- arnie (ravids girlfriend)
       </p>
       <p>
-        this site is an art piece which will document the journey which we will
+        This site is an art piece which will document the journey which we will
         go on together during the covid-19 pandemic. we started the isloation
         process on 19th of March 2020.
       </p>
+      <div className="note">
+        <p>
+          Note: <br /> 17 April
+          <br /> We have already noticed a major fact about documenting this
+          journey through COVID-19, that it is easy to document the good times.
+          But its much harder to photograph the diffucult times. the moments
+          when we are all down or doing nothing but hiding in our rooms all day
+          is much hard to capture.
+        </p>
+      </div>
     </div>
   );
 };
