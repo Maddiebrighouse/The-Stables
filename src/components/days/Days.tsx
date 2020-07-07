2f89029dd513c0566c4488afcbdd5ff770648826
Madeleine Brighouse-Mayo <madeleinebrighouse@gmail.com>
2020-07-08 09:09:53 +1200

    made number of days fixed to 53

--- src/components/days/Days.tsx
+++ src/components/days/Days.tsx
@@ -1,44 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import moment from "moment";
 import "./Days.scss";
 
 type State = {
-  listDays: Number[];
+  listDays: number[];
 };
 type Props = {};
-class Days extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      listDays: [],
-    };
-  }
+const Days = (props: Props, state: State) => {
+  const [listDays, setListDays] = React.useState([]);
 
-  componentDidMount() {
-    let list = [];
-    const startTime = "2020-03-19";
-    let todaysDate = moment(new Date());
-    let diffDays = todaysDate.diff(startTime, "days");
-    while (list.length <= diffDays - 1) {
+  useEffect(() => {
+    let list: number[] = [];
+    while (list.length <= 53 - 1) {
       let add: any = list.length + 1;
       list.push(add);
     }
-    this.setState({ listDays: list });
-  }
-  render() {
-    return (
-      <div className="days-container">
-        {this.state.listDays.map((day: Number) => {
-          return (
-            <Link key={day} to={`days/${day}`} className="day-square">
-              {day}
-            </Link>
-          );
-        })}
-      </div>
-    );
-  }
-}
+    setListDays(list);
+  }, []);
+  return (
+    <div className="days-container">
+      {listDays.map((day: Number, i) => {
+        return (
+          <Link key={i} to={`days/${day}`} className="day-square">
+            {day}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Days;
